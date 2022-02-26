2b104a823fbd48f91caeffd19b9cc5791852853f
Toushik Banik <toushikbanikeducation@gmail.com>
2022-02-27 01:34:40 +0530

    V 2.0

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Center(child: Text('Ask Me Anything')),
+      ),
+      body: Container(),
+    );
+  }
+}
